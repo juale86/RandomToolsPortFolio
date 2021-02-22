7f5515363de81b0aee88c8fd412d32c6f1114d0d
Juan Manuel Aleman <juale86@gmail.com>
2021-02-21 23:28:34 -0300

    Menu Icons Added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,12 @@ import './style.css'
 
 const Navbar = () => {
     return (
-        <nav className='header'>
+        <nav className='header fr'>
             <NavLink to='/'>Link palgun lao</NavLink>
             Barra
+            <a href="https://drive.google.com/file/d/1vLlrhed1YwdsNcAn-nHdHsira1A722Fg/view?usp=sharing">CV</a>
         </nav>
     );
 }
+
 export default Navbar;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,17 @@
 import './style.css';
+
+const IconNames = ['bitcoin','calculadora','clima','hogar','portapapeles']
+const IconPaths = IconNames.map(x=>'Iconos/'+x+'.svg')
+
+const Iconos = () => {
+    const Icons = IconPaths.map((x,index) => <img key={index} className={"iconClass"} src={x} alt="insertar SVG con la etiqueta image" />)
+    return Icons;
+}
+
 const Menu = ()=>{
-    return (<div className="menu">Menu???</div>);
+    return (<div className="menu fc">
+            <Iconos />
+            </div>);
 }
 
 export default Menu;
